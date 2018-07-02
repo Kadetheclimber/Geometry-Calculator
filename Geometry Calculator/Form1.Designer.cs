a51afeac1c747b0c51f6d3f54102816915cc4da7
Kade Allen <kadeallenprofessional@gmail.com>
2018-07-02 17:46:43 +1200

    First Page Title And Desc

--- Geometry Calculator/Form1.Designer.cs
+++ Geometry Calculator/Form1.Designer.cs
@@ -28,13 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.StartPageTitle = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.StartPageTitle);
+            this.panel1.Location = new System.Drawing.Point(8, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(472, 507);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // StartPageTitle
+            // 
+            this.StartPageTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StartPageTitle.AutoSize = true;
+            this.StartPageTitle.Font = new System.Drawing.Font("Lato", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartPageTitle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.StartPageTitle.Location = new System.Drawing.Point(72, 5);
+            this.StartPageTitle.Name = "StartPageTitle";
+            this.StartPageTitle.Size = new System.Drawing.Size(308, 39);
+            this.StartPageTitle.TabIndex = 0;
+            this.StartPageTitle.Text = "Geometry Calculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lato", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(417, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "A Place to Calculate Area, Volume And More";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(492, 523);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label StartPageTitle;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Geometry Calculator/Form1.cs
+++ Geometry Calculator/Form1.cs
@@ -16,5 +16,10 @@ namespace Geometry_Calculator
         {
             InitializeComponent();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
